e404cf309f1aec4e7d9dec3ff92684b1df986ca7
Jonathan Narzo <jonathan.narzo@gmail.com>
2014-10-19 21:40:24 +0800

    adding setting app id, app secret and redirect page

--- app.php
+++ app.php
@@ -7,4 +7,17 @@ require_once 'config.php';
 Facebook\FacebookSession::setDefaultApplication(APP_ID, APP_SECRET);
 $fb = new Facebook\FacebookRedirectLoginHelper(LOGIN_REDIRECT_PAGE);
 
+if($session = $fb->getSessionFromRedirect())
+{
+	$_SESSION['facebook'] = $session->getToken();
+	header('Location: index.php');
+}
+if(isset($_SESSION['facebook']))
+{
+	$session = new Facebook\FacebookSession($_SESSION['facebook']);
+	$request = new Facebook\FacebookRequest($session, 'GET', '/me');
+	$request = $request->execute();
+	$user = $request->getGraphObject()->asArray();
+}
+
 ?>
